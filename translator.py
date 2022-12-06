bbedc50b0abe7e81db9bc47158610aaa281ce2f1
João Righetto <jm_zr@hotmail.com>
2022-12-06 09:47:41 +0000

    Add translate to all languages simultaneously and save results to file

--- translator.py
+++ translator.py
@@ -2,6 +2,31 @@ import requests
 from bs4 import BeautifulSoup
 
 
+def find_translations(soup_obj):
+    translations_ = []
+    for translation_ in soup_obj.find_all(class_="display-term"):
+        translations_.append(translation_.text)
+    return translations_
+
+
+def find_examples(soup_obj):
+    examples_ = []
+    for div in soup_obj.find("section", {"id": "examples-content"}).find_all("div", {"class": "example"}):
+        for span in div.find_all("span", {"class": "text"}):
+            examples_.append(span.text.strip())
+    return examples_
+
+
+def make_soup(link):
+    h = {'User-Agent': 'Mozilla/5.0'}
+    r = requests.get(link, headers=h)
+    try:
+        s = BeautifulSoup(r.content, "html.parser")
+    except requests.exceptions.RequestException as e:
+        raise SystemExit(e)
+    return s
+
+
 supported_languages = {1: "Arabic", 2: "German", 3: "English", 4: "Spanish", 5: "French", 6: "Hebrew", 7: "Japanese",
                        8: "Dutch", 9: "Polish", 10: "Portuguese", 11: "Romanian", 12: "Russian", 13: "Turkish"}
 
@@ -10,36 +35,59 @@ for key, pair in supported_languages.items():
     print(f"{key}. {pair}")
 
 language_from = int(input("Type the number of your language:"))
-language_to = int(input("Type the number of language you want to translate to:"))
+language_to = int(input("Type the number of a language you want to translate to or '0' to translate to all languages:"))
 word_to_translate = input("Type the word you want to translate:")
 
+if language_to == 0:
+    with open(f"{word_to_translate}.txt", "w") as file:
+        for target_language in supported_languages.values():
+            if target_language != supported_languages[language_from]:
 
-url = f"https://context.reverso.net/translation/{supported_languages[language_from].casefold()}-{supported_languages[language_to].casefold()}/{word_to_translate}"
+                url = f"https://context.reverso.net/translation/{supported_languages[language_from].casefold()}-" \
+                      f"{target_language.casefold()}/{word_to_translate}"
 
-headers = {'User-Agent': 'Mozilla/5.0'}
-page = requests.get(url, headers=headers)
+                soup = make_soup(url)
 
-if page.status_code == 200:
+                translations = find_translations(soup)
+                examples = find_examples(soup)
 
-    soup = BeautifulSoup(page.content, "html.parser")
-    print(f"{supported_languages[language_to]} Translations:")
-    words = []
-    for word in soup.find_all(class_="display-term"):
-        words.append(word.text)
-    for word in words:
-        print(word)
+                print(f"{target_language} Translations:\n{translations[0]}\n")
+                print(f"{target_language} Examples:\n{examples[0]}\n{examples[1]}\n")
 
-    print()
+                file.write(f"{target_language} Translations:\n{translations[0]}\n\n")
+                file.write(f"{target_language} Examples:\n{examples[0]}\n{examples[1]}\n\n")
 
-    print(f"{supported_languages[language_to]} Examples:")
-    examples = []
-    section = soup.find("section", {"id": "examples-content"})
-    for div in section.find_all("div", {"class": "example"}):
-        for span in div.find_all("span", {"class": "text"}):
-            examples.append(span.text.strip())
-    for i in range(0, len(examples), 2):
-        print(examples[i])
-        print(examples[i+1])
-        print()
 else:
-    print(f"Error {page.status_code}, try again.")
+    url = f"https://context.reverso.net/translation/{supported_languages[language_from].casefold()}-" \
+          f"{supported_languages[language_to].casefold()}/{word_to_translate}"
+    soup = make_soup(url)
+    with open(f"{word_to_translate}.txt", "w") as file:
+
+        print(f"{supported_languages[language_to]} Translations:")
+        translations = find_translations(soup)
+
+        file.write(f"{supported_languages[language_to]} Translations:\n")
+        file.write("\n".join(translations))
+        file.write("\n\n")
+
+        for translation in translations:
+            print(translation)
+
+        print()
+
+        file.write(f"{supported_languages[language_to]} Examples:\n")
+
+        print(f"{supported_languages[language_to]} Examples:")
+        examples = find_examples(soup)
+        for i in range(0, len(examples), 2):
+            print(f"{examples[i]}\n{examples[i+1]}\n")
+            file.write(examples[i])
+            file.write("\n")
+            file.write(examples[i+1])
+            file.write("\n\n")
+
+
+
+
+
+
